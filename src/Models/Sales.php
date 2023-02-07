75c83891913656123788de58f93eb7e32c0e87ae
Gilbert Rutatiina <rutatiina@gmail.com>
2023-02-07 13:12:34 +0300

    Update scoping

--- src/Models/Sales.php
+++ src/Models/Sales.php
@@ -3,6 +3,7 @@
 namespace Rutatiina\Sales\Models;
 
 use Bkwld\Cloner\Cloneable;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 use Rutatiina\Tenant\Scopes\TenantIdScope;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -88,13 +89,37 @@ class Sales extends Model
                 $row->restore();
              });
         });
-
+    }
+    
+    /**
+     * Scope a query to only include approved records users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeApproved($query)
+    {
+        return $query->where('status', 'approved');
+    }
+    
+    /**
+     * Scope a query to only include not canceled records
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeNotCancelled($query)
+    {
+        return $query->where(function($q) {
+            $q->where('canceled', 0);
+            $q->orWhereNull('canceled');
+        });
     }
 
     public function rgGetAttributes()
     {
         $attributes = [];
-        $describeTable =  \DB::connection('tenant')->select('describe ' . $this->getTable());
+        $describeTable =  DB::connection('tenant')->select('describe ' . $this->getTable());
 
         foreach ($describeTable  as $row) {
 
